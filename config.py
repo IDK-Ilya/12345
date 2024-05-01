f3f76f5baaf9cf3821e0abf533618470eb42da26
Mixfix <mixfix2001@gmail.com>
2024-05-01 12:26:50 +0300

    Нахуевертил разного

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
 from dotenv import load_dotenv
 import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    CMC_API_KEY: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
 
 load_dotenv()
 
--- main.py
+++ main.py
@@ -3,11 +3,68 @@ from enum import Enum
 from typing import List, Optional
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi_users import FastAPIUsers
 from pydantic import BaseModel, Field
+from starlette.middleware.cors import CORSMiddleware
+
+from Auth.Auth import auth_backend
+from Auth.database import User
+from Auth.manager import get_user_manager
+from Auth.schemas import UserRead, UserCreate
+from router import router as router_crypto
 
 app = FastAPI(title="App test")
 
+# app.include_router(router_crypto)
+#
+#
+# origins = [
+#     "http://localhost:5432",
+#     "http://127.0.0.1:5432",
+# ]
+#
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+
+
+
+#-----------------------------------------------------------------------
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+current_user = fastapi_users.current_user()
+
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.username}"
+
+
+@app.get("/unprotected-route")
+def unprotected_route():
+    return f"Hello, anonym"
+
+#-----------------------------------------------------------------------
 
 fake_users = [
     {"id": 1, "role": "admin", "name": "Bob"},
@@ -26,13 +83,13 @@ class Degree(BaseModel):
     created_at: datetime
     type_degree: DegreeType
 
-class User(BaseModel):
+class User2(BaseModel):
     id: int
     role: str
     name: str
     degree: Optional[List[Degree]] = []
 
-@app.get("/users/{user_id}", response_model=List[User])
+@app.get("/users/{user_id}", response_model=List[User2])
 def get_user(user_id: int):
     return [user for user in fake_users if user.get("id") == user_id]
 
@@ -68,6 +125,20 @@ class Trade(BaseModel):
 def add_trades(trades: List[Trade]):
     fake_trades.extend(trades)
     return {"status": 200, "date":fake_trades}
+
+#-----------------------------------------------------------------------
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["*"], #Обязательно прописать хедеры которые есть
+)
 
 if __name__=="__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
